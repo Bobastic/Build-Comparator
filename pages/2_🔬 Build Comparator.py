7344c0636a70797dd4e276cece3dedb296ea9394
Smaxy63 <63102199+Smaxy63@users.noreply.github.com>
2023-09-01 16:04:20 +0200

    Update 2_🔬 Build Comparator.py

--- pages/2_🔬 Build Comparator.py
+++ pages/2_🔬 Build Comparator.py
@@ -30,20 +30,22 @@ st.markdown("""
     </style>
     """,unsafe_allow_html=True)
 
-cols=st.columns(6)
+cols=st.columns(7)
 with cols[0]:
     weaponLvl=st.number_input("Weapon Level",0,25,25)
-    opaline=st.toggle("Opaline Hardtear",value=True,help="Opponent has +15% negations.")
 with cols[1]:
+    opaline=st.toggle("Opaline Hardtear",value=True,help="Opponent has +15% negations.")
+    aaa=st.toggle("a a",value=True,help="Opponent has +15% negations.")
+with cols[2]:
     weaponBuffs=st.toggle("Weapon buffs",value=True,help="Grease (Lightning except for split damage weapons like Treespear), Flaming Strike, Lightning Slash, Sacred Blade.")
     counterHits=st.toggle("Counter Hits",value=True,help="+15%: normal counter hit. +32%: counter hit with Spear Talisman equipped.")
-with cols[2]:
+with cols[3]:
     multicolor=st.toggle("Multicolor",value=True,help="One color per build or simple gradient.")
     showWeaponClass=st.toggle("Weapon Class",value=False,help="Display weapon class on the left of the table.")
-with cols[3]:
+with cols[4]:
     displayPercentage=st.toggle("Ratio with best",value=True,help="How much worse the weapon is compared to the best. For example -20% means the weapon deals 20% less damage than the best.")
     showStats=st.toggle("Build Stats",value=True,help="Show stats in column header.")
-with cols[4]:
+with cols[5]:
     comparison=st.selectbox("Comparison with best of...",("row","class","all"),index=1,
                             help="Row: tells you what is the best infusion and build for the weapon. Class: tells you what is the best weapon of the class. All: tells you whats is the best weapon of the table")
 
@@ -77,7 +79,7 @@ if st.session_state.nBuilds!=0 and len(st.session_state.weapons)!=0:
         table=DMGtable(weapons,builds,infusions,defenses,negations,weaponBuffs=weaponBuffs,counterHits=counterHits)
         fancy=fancyTable(table,comparison=comparison,displayPercentage=displayPercentage,showStats=showStats,multicolor=multicolor,showWeaponClass=showWeaponClass)
         st.write(fancy.to_html(),unsafe_allow_html=True)
-    with cols[5]:
+    with cols[6]:
         st.download_button("Download CSV",table.to_csv(),file_name="buildComparatorData.csv")
         if not st.session_state.download:
             st.button("Convert to PNG",disabled=True,on_click=convertPNG)
