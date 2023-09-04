672a3f0d7ad7df054347839d0f89ad559fc29606
Smaxy63 <63102199+Smaxy63@users.noreply.github.com>
2023-09-04 09:15:40 +0200

    Update 👋_Info.py

--- 👋_Info.py
+++ 👋_Info.py
@@ -2,13 +2,16 @@ import streamlit as st
 
 st.set_page_config(layout='centered',page_title="Build Comparator",page_icon="⚖️")
 
+def justify(text):
+  return st.markdown(f"<div style='text-align: justify;'>{text}</div>",unsafe_allow_html=True)
+
 st.sidebar.info("Hi. Check this page for some information about this tool or head straight to **Build Comparator** to figure it out by yourself.")
 
 st.header("Hi. This is my Build Comparator.")
 
 st.subheader("What is the purpose of this tool?")
 
-st.markdown("""
+justify("""
 There are already lots of tools for build optimization, mainly in the form of spreadsheets or slugbot.
 However, I could not find a way to compare whole builds together.
 Usually you get tools for optimal infusion, optimal stats but always for one weapon or weapon class.
@@ -17,7 +20,7 @@ There isn't to my knowledge a way to see the damage values of a **whole inventor
 
 st.subheader("Use case examples")
 
-st.markdown("""
+justify("""
 - How much more damage do I get with 69 STR compared to 54 STR on all these weapons?
 - Is Cold->Keen better than Cold->Magic for this inventory?
 - Fire + Flaming Strike buff or Heavy + Lightning Grease?
@@ -27,7 +30,7 @@ st.markdown("""
 
 st.subheader("So what does it actually do?")
 
-st.markdown("""
+justify("""
 You input a list of **weapons** of your choice and as many **builds** and **infusions** as you like and you get a table with estimated damage that is widely customizable.
 You can display counter damage, comparisons with the best infusion etc...
 You can also export raw data as a CSV file or download the table as a PNG image (WIP) to get a cleaner result than screenshots.
@@ -35,7 +38,7 @@ You can also export raw data as a CSV file or download the table as a PNG image 
 
 st.subheader("Ok, how do I use it?")
 
-st.markdown("""
+justify("""
 Input your weapons, builds, infusions and enemy stats in **🛠️ Parameters**.
 Then go to **🔬 Build Comparator** to show the table and change what information you want to display.
 There are already some default weapons and builds so you can go straight to **🔬 Build Comparator** to see what the table looks like.
@@ -43,6 +46,6 @@ There are already some default weapons and builds so you can go straight to **�
 
 st.subheader("I would like to give some feedback.")
 
-st.markdown("""
+justify("""
 Whether it is for functionnality request, default values suggestions or anything else really, feel free to message  **.smaxy.** on Discord.
 """)
