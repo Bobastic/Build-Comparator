27acb40d6a03d2d977e43edcaf7dc6babe483cc2
Smaxy63 <63102199+Smaxy63@users.noreply.github.com>
2023-09-05 11:01:19 +0200

    Update 2_Weapon Info.py

--- pages/2_Weapon Info.py
+++ pages/2_Weapon Info.py
@@ -16,39 +16,38 @@ with cols[2]: st.number_input("INT",1,99,9,key="INT")
 with cols[3]: st.number_input("FTH",1,99,9,key="FTH")
 with cols[4]: st.number_input("ARC",1,99,7,key="ARC")
 
-info,allocate,bic=st.tabs(3)
+info,allocate,bic=st.tabs(["Weapon Info","Allocate Stats","Best of class"])
 
-with 
-cols=st.columns(3)
-with cols[0]: st.selectbox("Weapon class",weaponClasses,key="class")
-with cols[1]: st.selectbox("Weapon",weaponsOfClass(st.session_state["class"]),key="weapon",index=3)
-with cols[2]: st.selectbox("Infusion",baseInfusions,key="infusion")
-
-cols=st.columns(8)
-with cols[0]: st.number_input("Standard defense",0,400,st.session_state.defstandard)
-with cols[1]: st.number_input("Strike defense",0,400,st.session_state.defstrike)
-with cols[2]: st.number_input("Slash defense",0,400,st.session_state.defslash)
-with cols[3]: st.number_input("Pierce defense",0,400,st.session_state.defpierce)
-with cols[4]: st.number_input("Magic defense",0,400,st.session_state.defmagic)
-with cols[5]: st.number_input("Fire defense",0,400,st.session_state.deffire)
-with cols[6]: st.number_input("Lightning defense",0,400,st.session_state.deflightning)
-with cols[7]: st.number_input("Holy defense",0,400,st.session_state.defholy)
-cols=st.columns(8)
-with cols[0]: st.number_input("Standard negation",0.,100.,st.session_state.negstandard,format="%.1f")
-with cols[1]: st.number_input("Strike negation",0.,100.,st.session_state.negstrike,format="%.1f")
-with cols[2]: st.number_input("Slash negation",0.,100.,st.session_state.negslash,format="%.1f")
-with cols[3]: st.number_input("Pierce negation",0.,100.,st.session_state.negpierce,format="%.1f")
-with cols[4]: st.number_input("Magic negation",0.,100.,st.session_state.negmagic,format="%.1f")
-with cols[5]: st.number_input("Fire negation",0.,100.,st.session_state.negfire,format="%.1f")
-with cols[6]: st.number_input("Lightning negation",0.,100.,st.session_state.neglightning,format="%.1f")
-with cols[7]: st.number_input("Holy negation",0.,100.,st.session_state.negholy,format="%.1f")
-
-dmg=ARcalculator(st.session_state.weapon,st.session_state.infusion,[st.session_state.STR,st.session_state.DEX,st.session_state.INT,st.session_state.FTH,st.session_state.ARC])
-labels=[l for i,l in enumerate(dmgTypes) if dmg[i]!=0]
-sizes=[s for s in dmg if s!=0]
-fig1,ax1=plt.subplots()
-ax1.pie(sizes,labels=labels,autopct='%1.1f%%',labeldistance=None)
-#ax1.axis('equal')
-
-st.pyplot(fig1)
+with info:
+    cols=st.columns(3)
+    with cols[0]: st.selectbox("Weapon class",weaponClasses,key="class")
+    with cols[1]: st.selectbox("Weapon",weaponsOfClass(st.session_state["class"]),key="weapon",index=3)
+    with cols[2]: st.selectbox("Infusion",baseInfusions,key="infusion")
+    cols=st.columns(8)
+    with cols[0]: st.number_input("Standard defense",0,400,st.session_state.defstandard)
+    with cols[1]: st.number_input("Strike defense",0,400,st.session_state.defstrike)
+    with cols[2]: st.number_input("Slash defense",0,400,st.session_state.defslash)
+    with cols[3]: st.number_input("Pierce defense",0,400,st.session_state.defpierce)
+    with cols[4]: st.number_input("Magic defense",0,400,st.session_state.defmagic)
+    with cols[5]: st.number_input("Fire defense",0,400,st.session_state.deffire)
+    with cols[6]: st.number_input("Lightning defense",0,400,st.session_state.deflightning)
+    with cols[7]: st.number_input("Holy defense",0,400,st.session_state.defholy)
+    cols=st.columns(8)
+    with cols[0]: st.number_input("Standard negation",0.,100.,st.session_state.negstandard,format="%.1f")
+    with cols[1]: st.number_input("Strike negation",0.,100.,st.session_state.negstrike,format="%.1f")
+    with cols[2]: st.number_input("Slash negation",0.,100.,st.session_state.negslash,format="%.1f")
+    with cols[3]: st.number_input("Pierce negation",0.,100.,st.session_state.negpierce,format="%.1f")
+    with cols[4]: st.number_input("Magic negation",0.,100.,st.session_state.negmagic,format="%.1f")
+    with cols[5]: st.number_input("Fire negation",0.,100.,st.session_state.negfire,format="%.1f")
+    with cols[6]: st.number_input("Lightning negation",0.,100.,st.session_state.neglightning,format="%.1f")
+    with cols[7]: st.number_input("Holy negation",0.,100.,st.session_state.negholy,format="%.1f")
+    
+    dmg=ARcalculator(st.session_state.weapon,st.session_state.infusion,[st.session_state.STR,st.session_state.DEX,st.session_state.INT,st.session_state.FTH,st.session_state.ARC])
+    labels=[l for i,l in enumerate(dmgTypes) if dmg[i]!=0]
+    sizes=[s for s in dmg if s!=0]
+    fig1,ax1=plt.subplots()
+    ax1.pie(sizes,labels=labels,autopct='%1.1f%%',labeldistance=None)
+    #ax1.axis('equal')
+    
+    st.pyplot(fig1)
 
