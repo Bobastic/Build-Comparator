fb98f9dc0470115a85fb5a963b7d91fa9637e802
Smaxy63 <63102199+Smaxy63@users.noreply.github.com>
2023-09-05 15:35:13 +0200

    Update 2_Weapon Info.py

--- pages/2_Weapon Info.py
+++ pages/2_Weapon Info.py
@@ -81,7 +81,7 @@ with info:
     fig = go.Figure(go.Bar(x=x, y=[2,5,1,9], name='Montreal'))
     fig.add_trace(go.Bar(x=x, y=[1, 4, 9, 16], name='Ottawa'))
     fig.add_trace(go.Bar(x=x, y=[6, 8, 4.5, 8], name='Toronto'))
-    fig.update_layout(barmode='stack', xaxis={'categoryorder':'total descending'})
+    fig.update_layout(barmode='stack',xaxis={'categoryorder':'total descending'},orientation="h")
     st.plotly_chart(fig)
 
 with allocate:
