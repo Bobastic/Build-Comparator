d6e9df2c9ea989e5c8df0809ba982c1f082ce336
Smaxy63 <63102199+Smaxy63@users.noreply.github.com>
2023-09-05 16:43:17 +0200

    Update 2_Weapon Info.py

--- pages/2_Weapon Info.py
+++ pages/2_Weapon Info.py
@@ -50,11 +50,12 @@ with info:
     with cols[7]: st.number_input("Holy negation",0.,100.,st.session_state.negholy,format="%.1f")
     st.divider()
     data=np.array([ARcalculator(st.session_state.weapon,i,[st.session_state.STR,st.session_state.DEX,st.session_state.INT,st.session_state.FTH,st.session_state.ARC]) for i in baseInfusions])
+    labels=[baseInfusions[i] for i in range(len(baseInfusions)).sort(key=lambda x:sum(data[x,:]))]
     data=data[np.argsort(data.sum(axis=1))[:10]] # we sort by total and keep the top 10
     colors=["rgb(240, 242, 246)","rgb(240, 242, 246)","rgb(240, 242, 246)","rgb(240, 242, 246)","rgb(14, 90, 157)","rgb(214, 39, 40)","rgb(255, 225, 53)","rgb(255, 127, 14)"]
     fig=go.Figure()
     for i in range(8):
-        fig.add_trace(go.Bar(y=baseInfusions,x=data[:,i],name=dmgTypes[i],orientation="h",text=data[:,i],marker={"color":colors[i]}))
+        fig.add_trace(go.Bar(y=labels,x=data[:,i],name=dmgTypes[i],orientation="h",text=data[:,i],marker={"color":colors[i]}))
     fig.update_layout(barmode='stack')
     st.plotly_chart(fig)
 
