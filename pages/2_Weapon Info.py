baa558e0c7c80c7c1b01d1eac41f884e3e3ad593
Smaxy63 <63102199+Smaxy63@users.noreply.github.com>
2023-09-05 15:40:41 +0200

    Update 2_Weapon Info.py

--- pages/2_Weapon Info.py
+++ pages/2_Weapon Info.py
@@ -78,9 +78,9 @@ with info:
     
     import plotly.graph_objects as go
     x=['b', 'a', 'c', 'd']
-    fig = go.Figure(go.Bar(x=x, y=[2,5,1,9], name='Montreal',orientation="h"))
-    fig.add_trace(go.Bar(x=x, y=[1, 4, 9, 16], name='Ottawa',orientation="h"))
-    fig.add_trace(go.Bar(x=x, y=[6, 8, 4.5, 8], name='Toronto',orientation="h"))
+    fig = go.Figure(go.Bar(y=x, x=[2,5,1,9], name='Montreal',orientation="h"))
+    fig.add_trace(go.Bar(y=x, x=[1, 4, 9, 16], name='Ottawa',orientation="h"))
+    fig.add_trace(go.Bar(y=x, x=[6, 8, 4.5, 8], name='Toronto',orientation="h"))
     fig.update_layout(barmode='stack',xaxis={'categoryorder':'total descending'})
     st.plotly_chart(fig)
 
