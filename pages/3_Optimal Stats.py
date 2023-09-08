6ad3b353cd3721fd03bba0f3ebe1a98ef47db359
Smaxy63 <63102199+Smaxy63@users.noreply.github.com>
2023-09-08 10:12:12 +0200

    Update 3_Optimal Stats.py

--- pages/3_Optimal Stats.py
+++ pages/3_Optimal Stats.py
@@ -95,4 +95,4 @@ with bestStats:
     cols=st.columns(2)
     with cols[0]: st.selectbox("Infusion",baseInfusions,key="infusion__",on_change=updateState,args=("infusion__",))
     with cols[1]: st.number_input("Stat points to allocate",0,813,st.session_state.pts,key="pts_",on_change=updateState,args=("pts_",))
-    st.info("A Vagabond with 60 vigor, 10 mind and 27 endurance has 55 points left to allocate to reach RL 125.")
+    with cols[2]: st.info("A Vagabond with 60 vigor, 10 mind and 27 endurance has 55 points left to allocate to reach RL 125.")
