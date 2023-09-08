2ca8f470e2dc28210be52da7e8c856c540ab6168
Smaxy63 <63102199+Smaxy63@users.noreply.github.com>
2023-09-08 10:43:36 +0200

    Update 3_Optimal Stats.py

--- pages/3_Optimal Stats.py
+++ pages/3_Optimal Stats.py
@@ -81,7 +81,8 @@ with bestInf:
         negations=[st.session_state.negstandard,st.session_state.negstrike,st.session_state.negslash,st.session_state.negpierce,
                   st.session_state.negmagic,st.session_state.negfire,st.session_state.neglightning,st.session_state.negholy]
         nBest=20
-        data=np.array([ARtoDMG(ARcalculator(st.session_state.weapon,i,stats),defenses,negations) for i in baseInfusions])
+        weapon=f"{'2H ' if st.session_state.twoH_ else ''}{st.session_state.weapon}"
+        data=np.array([ARtoDMG(ARcalculator(weapon,i,stats),defenses,negations) for i in baseInfusions])
         labels=[baseInfusions[i] for i in sorted(range(len(baseInfusions)),key=lambda x:sum(data[x,:]))][-nBest:]
         data=data[np.argsort(data.sum(axis=1))[-nBest:]] # we sort by total and keep the best
         colors=["rgb(240, 242, 246)","rgb(240, 242, 246)","rgb(240, 242, 246)","rgb(240, 242, 246)","rgba(14, 90, 157, 0.3)","rgba(214, 39, 40, 0.3)","rgba(255, 225, 53, 0.3)","rgba(255, 127, 14, 0.3)"]
