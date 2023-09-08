587128e9e13fe5ed8e7832a486cdfdaaca210d03
Smaxy63 <63102199+Smaxy63@users.noreply.github.com>
2023-09-08 16:12:25 +0200

    Update 3_Optimal Stats.py

--- pages/3_Optimal Stats.py
+++ pages/3_Optimal Stats.py
@@ -31,7 +31,7 @@ st.markdown("""
 
 cols=st.columns([8,8,1])
 with cols[0]: st.selectbox("Weapon class",weaponClasses,key="wClass")
-with cols[1]: st.selectbox("Weapon",weaponsOfClass(st.session_state["wClass"]),index=3,key="weapon")
+with cols[1]: st.selectbox("Weapon",weaponsOfClass(st.session_state["wClass"]),key="weapon")
 with cols[2]: st.toggle("2H",key="twoH_")
 cols=st.columns(8)
 with cols[0]: st.number_input("Standard defense",0,400,st.session_state.defstandard,key="defstandard_",on_change=updateState,args=("defstandard_",))
