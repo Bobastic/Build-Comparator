637feacbbd7f5e4b567599fe517732c9741a9982
Smaxy63 <63102199+Smaxy63@users.noreply.github.com>
2023-09-08 13:37:56 +0200

    Update constants.py

--- utils/constants.py
+++ utils/constants.py
@@ -1,1 +1,30 @@
+dmgTypes=["Standard","Strike","Slash","Pierce","Magic","Fire","Lightning","Holy"]
+baseInfusions=["Heavy","Fire","Keen","Lightning","Magic","Cold","Sacred","Flame Art","Blood","Occult","Standard"]
+infusionOrder=["Heavy","Hvy+Gse","Fire","Fire+FS","Keen","Keen+Gse","Lightning","Ltng+LS","Magic","Cold","Sacred","Scrd+SB","Flame Art","F.Art+FS","Blood","Poison","Occult","Standard","Std+Gse"]
 
+idWeaponClass={
+    29: "Halberds",
+    25: "Spears",
+    3: "Straight Swords",
+    1: "Daggers",
+    9: "Curved Swords",
+    5: "Greatswords",
+    7: "Colossal Swords",
+    15: "Thrusting Swords",
+    16: "Heavy Thrusting Swords",
+    11: "Curved Greatswords",
+    13: "Katanas",
+    14: "Twinblades",
+    21: "Hammers",
+    23: "Greathammers",
+    24: "Flails",
+    17: "Axes",
+    19: "Greataxes",
+    28: "Great Spears",
+    31: "Scythes",
+    39: "Whips",
+    35: "Fists",
+    37: "Claws",
+    41: "Colossal Weapons",
+}
+weaponClasses=idWeaponClass.values()
