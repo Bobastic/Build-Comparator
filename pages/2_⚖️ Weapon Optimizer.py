b6acc3182bb39b0d2229eaa423decde80ebf4725
Smaxy63 <63102199+Smaxy63@users.noreply.github.com>
2023-09-11 10:52:45 +0200

    Update 2_⚖️ Weapon Optimizer.py

--- pages/2_⚖️ Weapon Optimizer.py
+++ pages/2_⚖️ Weapon Optimizer.py
@@ -108,7 +108,7 @@ with bestStats:
     with cols[3]: st.number_input("Base FTH",1,99,st.session_state.baseFTH,key="baseFTH_",on_change=updateState,args=("baseFTH_",))
     with cols[4]: st.number_input("Base ARC",1,99,st.session_state.baseARC,key="baseARC_",on_change=updateState,args=("baseARC_",))
     cols=st.columns(3)
-    with cols[0]: st.selectbox("Infusion",baseInfusions,key="infusion__",on_change=updateState,args=("infusion__",))
+    with cols[0]: infusion=st.selectbox("Infusion",baseInfusions,key="infusion__",on_change=updateState,args=("infusion__",))
     with cols[1]: pts=st.number_input("Stat points to allocate",0,813,st.session_state.pts,key="pts_",on_change=updateState,args=("pts_",))
     with cols[2]: st.info("A base Vagabond with 60 VIG and 27 END has 55 points left for RL 125.")
     stats_=[st.session_state.baseSTR,st.session_state.baseDEX,st.session_state.baseINT,st.session_state.baseFTH,st.session_state.baseARC]
@@ -118,8 +118,8 @@ with bestStats:
             tmpStats=stats_[:]
             tmpStats[i]+=1
             tmpDmg=ARtoDMG(ARcalculator(weapon,st.session_state.infusion,tmpStats),defenses,negations)
-            if tmpDmg>dmg:
-                dmg=tmpDmg
+            if sum(tmpDmg)>dmg:
+                dmg=sum(tmpDmg)
                 lvlUp=i
         stats[lvlUp]+=1
         pts-=1
